4c66941a0da684026ef434d07090289c8ae880b7
fmartin <fmartin@palermo.com.ar>
2018-12-04 18:01:55 -0300

    clone project

---
+++ start/routes.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const Route = use('Route')
+
+Route.get('images/:path', 'ImageController.show')
+
+Route.post('/users', 'UserController.create')
+Route.post('/sessions', 'SessionController.create')
+Route.post('properties/:id/images', 'ImageController.store').middleware('auth')
+
+Route.resource('properties', 'PropertyController')
+.apiOnly()
+.middleware('auth')
